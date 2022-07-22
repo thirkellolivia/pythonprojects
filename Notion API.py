8743f6cc8c73ac8be18cd261b46d753c83e7ffce
Olivia Thirkell <100705675+thirkellolivia@users.noreply.github.com>
2022-07-22 14:56:13 +0100

    Add files via upload

---
+++ Notion API.py
@@ -0,0 +1,26 @@
+# import packages
+import json
+import requests
+
+# set up internal integration key
+# set up database id (integration must have access to database)
+# set up authorisation key
+token = 'secret_aexh37sHbPELr0pOhb5v228Z4MbUV7BtqpaNLiuwqWB'
+
+database_id = '12c59c74296d41f0b2ed22661ab0499d'
+
+headers = {"Authorization": f"Bearer {token}", "Notion-Version": "2022-06-28"}
+
+# define function to return database.
+# status code 200 indicates successful http request
+def readDatabase(database_id, headers):
+    readUrl = f"https://api.notion.com/v1/databases/{database_id}/query"
+
+    res = requests.request("POST", readUrl, headers=headers)
+    data = res.json()
+    print(res.status_code)
+
+    with open('./db.json', 'w', encoding='utf8') as f:
+        json.dump(data, f, ensure_ascii=False)
+
+readDatabase(database_id, headers)
